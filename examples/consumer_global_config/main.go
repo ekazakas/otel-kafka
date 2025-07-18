d0955813fbcef7860ee286dade02fd73420f55c7
Eduardas Kazakas <eduardas.kazakas@vinted.com>
2025-07-18 17:39:23 +0300

    chore(examples): added examples with global OTEL config

---
+++ examples/consumer_global_config/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	otelkafka "github.com/ekazakas/otel-kafka"
+	"github.com/ekazakas/otel-kafka/examples"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	successLog := log.New(os.Stdout, "[otel-kafka]", log.LstdFlags)
+	failureLog := log.New(os.Stderr, "[otel-kafka]", log.LstdFlags)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	serviceName := "example-consumer"
+
+	tracerProvider, err := examples.CreateTracer(serviceName)
+	if err != nil {
+		failureLog.Panicf("Failed to create tracer provider: %s", err)
+	}
+	defer func() {
+		successLog.Println("Shutting down TracerProvider")
+
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			failureLog.Panicf("Failed to shutdown TracerProvider: %s", err)
+		}
+
+		successLog.Println("TracerProvider shutdown complete")
+	}()
+
+	meterProvider, err := examples.CreateMeterProvider(serviceName)
+	if err != nil {
+		failureLog.Panicf("Failed to create meter provider: %s", err)
+	}
+	defer func() {
+		successLog.Println("Shutting down MeterProvider")
+
+		if err := meterProvider.Shutdown(ctx); err != nil {
+			failureLog.Panicf("Failed to shutdown MeterProvider: %s", err)
+		}
+
+		successLog.Println("MeterProvider shutdown complete")
+	}()
+
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetMeterProvider(meterProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	consumer, err := otelkafka.NewConsumer(
+		kafka.ConfigMap{
+			"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
+			"group.id":          "example-consumer-group",
+			"auto.offset.reset": "earliest",
+		},
+	)
+	if err != nil {
+		failureLog.Panicf("Failed to create Consumer: %s", err)
+	}
+	defer func() {
+		successLog.Println("Shutting down Consumer")
+
+		if err := consumer.Close(); err != nil {
+			log.Panicf("Failed to close Consumer: %s", err)
+		}
+
+		successLog.Println("Consumer shutdown complete")
+	}()
+
+	if err := consumer.SubscribeTopics([]string{os.Getenv("KAFKA_TOPIC")}, rebalanceCallback(successLog)); err != nil {
+		log.Panicf("Failed to subscribe to topics: %s", err)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go examples.ServeMetrics(ctx, 2223, successLog, failureLog)
+
+	successLog.Println("Starting consumer loop")
+
+	for {
+		select {
+		case sig := <-sigChan:
+			successLog.Printf("Received signal %s, shutting down...", sig)
+
+			return
+		default:
+			ev := consumer.Poll(1000)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				if e.TopicPartition.Error != nil {
+					failureLog.Printf("Reception failed for message: %s", e.TopicPartition.Error)
+				} else {
+					successLog.Printf("Received message %s from %s with offset %d.", e.Value, *e.TopicPartition.Topic, e.TopicPartition.Offset)
+				}
+			case kafka.Error:
+				failureLog.Printf("Error when polling messages: %s", e)
+			default:
+				successLog.Printf("Ignored message: %s", e)
+			}
+		}
+	}
+}
+
+func rebalanceCallback(logger *log.Logger) func(c *kafka.Consumer, event kafka.Event) error {
+	return func(c *kafka.Consumer, event kafka.Event) error {
+		switch ev := event.(type) {
+		case kafka.AssignedPartitions:
+			logger.Printf("Assigned partitions. Count: %d, protocol: %s", len(ev.Partitions), c.GetRebalanceProtocol())
+		case kafka.RevokedPartitions:
+			logger.Printf("Revoked partitions. Count: %d, protocol: %s", len(ev.Partitions), c.GetRebalanceProtocol())
+		default:
+			return fmt.Errorf("unexpected event: %v", event)
+		}
+
+		return nil
+	}
+}
---
+++ examples/producer_global_config/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	otelkafka "github.com/ekazakas/otel-kafka"
+	"github.com/ekazakas/otel-kafka/examples"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	successLog := log.New(os.Stdout, "[otel-kafka]", log.LstdFlags)
+	failureLog := log.New(os.Stderr, "[otel-kafka]", log.LstdFlags)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	serviceName := "example-consumer"
+
+	tracerProvider, err := examples.CreateTracer(serviceName)
+	if err != nil {
+		failureLog.Panicf("Failed to create TracerProvider: %s", err)
+	}
+	defer func() {
+		successLog.Println("Shutting down TracerProvider")
+
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			failureLog.Panicf("Failed to shutdown TracerProvider: %s", err)
+		}
+
+		successLog.Println("TracerProvider shutdown complete")
+	}()
+
+	meterProvider, err := examples.CreateMeterProvider(serviceName)
+	if err != nil {
+		failureLog.Panicf("Failed to create MeterProvider: %s", err)
+	}
+	defer func() {
+		successLog.Println("Shutting down MeterProvider")
+
+		if err := meterProvider.Shutdown(ctx); err != nil {
+			failureLog.Panicf("Failed to shutdown MeterProvider: %s", err)
+		}
+
+		successLog.Println("MeterProvider shutdown complete")
+	}()
+
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetMeterProvider(meterProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	producer, err := otelkafka.NewProducer(
+		kafka.ConfigMap{
+			"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
+		},
+	)
+	if err != nil {
+		failureLog.Panicf("Failed to create Producer: %s", err)
+	}
+	defer func() {
+		successLog.Println("Shutting down Producer")
+
+		producer.Close()
+
+		successLog.Println("Producer shutdown complete")
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go examples.ServeMetrics(ctx, 2224, successLog, failureLog)
+
+	topic := os.Getenv("KAFKA_TOPIC")
+
+	for i := 0; ; i++ {
+		select {
+		case <-sigChan:
+			return
+		default:
+			time.Sleep(1 * time.Second)
+
+			func() {
+				ctx, span := tracerProvider.Tracer("example-producer").Start(ctx, "produce message")
+				defer span.End()
+
+				deliveryChan := make(chan kafka.Event)
+				defer close(deliveryChan)
+
+				message := &kafka.Message{
+					TopicPartition: kafka.TopicPartition{
+						Topic:     &topic,
+						Partition: kafka.PartitionAny,
+					},
+					Value: []byte(fmt.Sprintf("message-%d", i)),
+					Key:   []byte(fmt.Sprintf("key-%d", i)),
+					Headers: []kafka.Header{
+						{
+							Key:   "header",
+							Value: []byte("header values are binary"),
+						},
+					},
+				}
+
+				otel.GetTextMapPropagator().Inject(ctx, otelkafka.NewMessageCarrier(message))
+
+				err = producer.Produce(message, deliveryChan)
+				if err != nil {
+					failureLog.Printf("Error producing message. Error: %s", err)
+
+					return
+				}
+
+				e := <-deliveryChan
+				if m, ok := e.(*kafka.Message); ok {
+					if m.TopicPartition.Error != nil {
+						failureLog.Printf("Delivery failed for message: %s", m.TopicPartition.Error)
+					} else {
+						successLog.Printf("Delivered message to topic %s partition %d at offset %d", *m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+					}
+				}
+			}()
+		}
+	}
+}
